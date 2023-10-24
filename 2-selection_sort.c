d17b35458a969d8861844d4380944bfbb92ef844
immaculateadhiambo79@gmail.com <immaculateadhiambo79@gmail.com>
2023-10-24 15:58:50 -0700

    Insertion

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * swap_ints - swap two ints in an array.
+ * @x: first int to swap.
+ * @y: second int to swap.
+ */
+void swap_ints(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * selection_sort - sort array of ints in ascending order by selection sort.
+ * @array: array of ints
+ * @size: size of array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t a, b;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		min = array + a;
+		for (b = a + 1; b < size; b++)
+			min = (array[b] < *min) ? (array + b) : min;
+
+		if ((array + a) != min)
+		{
+			swap_ints(array + a, min);
+			print_array(array, size);
+		}
+	}
+}
